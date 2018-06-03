36291f84fdc70fbd3c36df54a367a4eacdfaafe3
adi-hwn <adi.hwn@gmail.com>
2018-06-03 18:28:36 -0500

    bugfix

--- update.php
+++ update.php
@@ -4,7 +4,7 @@
 	mysql_query("USE userdb5");
 
 	$result = 0; // 0 - OK, 1 - Not Found, 2 - ID unspecified
-	if (isset($_POST[id]) isset($_POST[user_id]) and isset($_POST[product_id]) and isset($_POST[order_id]) and isset($_POST[rating]) and isset($_POST[review])) {
+	if (isset($_POST[id]) and isset($_POST[user_id]) and isset($_POST[product_id]) and isset($_POST[order_id]) and isset($_POST[rating]) and isset($_POST[review])) {
 		$id = mysql_real_escape_string($_POST[id]);
 
 		$uid = mysql_real_escape_string($_POST[user_id]);
